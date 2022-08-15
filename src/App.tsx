1963a4d7d873bd72533c7fc694f1cfe0b2a677b8
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-15 14:27:43 +0500

    routes switsh

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
+import AppRouter from './component/AppRouter'
 
 const App: FC = () => {
 	return (
 		<div>
-			Works
+			<AppRouter />
 		</div>
 	);
 };
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Login: FC = () => {
+	return (
+		<div>
+			Login page
+		</div>
+	);
+};
+
+export default Login;
