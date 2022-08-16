c0a43e6f1a2dd91190cd1db39ff5fcb9a69509e9
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-16 12:10:29 +0500

    login

--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -1,10 +1,21 @@
 import { Button, Form, Input } from 'antd';
 import React, { FC } from 'react';
+import { useDispatch } from 'react-redux';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { AuthActionCreators } from '../store/reducers/auth/action-creators';
 import { rules } from '../utils/rules';
 
 const LoginForm: FC = () => {
+	const dispatch = useDispatch()
+	const { error, isLoading } = useTypedSelector(state => state.auth)
+
+	const submit = () => {
+		console.log('try login')
+		dispatch(AuthActionCreators.login('user', '123'))
+	}
 	return (
-		<Form>
+		<Form
+			onFinish={submit}>
 			<Form.Item label="Username"
 				name="username"
 				rules={[rules.required("please input your username")]}>
---
+++ src/store/reducers/auth/action-creators.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { AppDispatch } from "../..";
+import { IUser } from "../../../models/IUser";
+import { AuthActionsEnum, SetAuthAction, SetErrorAction, SetIsLoadingAction, SetUserAction } from "./types";
+
+
+export const AuthActionCreators = {
+	setUser: (user: IUser): SetUserAction => ({ type: AuthActionsEnum.SET_USER, payload: user }),
+	setIsAuth: (auth: boolean): SetAuthAction => ({ type: AuthActionsEnum.SET_AUTH, payload: auth }),
+	setIsLoading: (payload: boolean): SetIsLoadingAction => ({ type: AuthActionsEnum.SET_IS_LOADING, payload: payload }),
+	setError: (payload: string): SetErrorAction => ({ type: AuthActionsEnum.SET_ERROR, payload: payload }),
+	login: (username: string, password: string) => async (dispatch: AppDispatch) => {
+		try {
+			dispatch(AuthActionCreators.setIsLoading(true))
+			setTimeout(async () => {
+
+				const response = await axios.get<IUser[]>('./users.json')
+				const mockUser = response.data.find(user => user.username === username && user.password === password)
+				if (mockUser) {
+					localStorage.setItem('auth', 'true')
+					localStorage.setItem('username', mockUser.username)
+					dispatch(AuthActionCreators.setIsAuth(true))
+					dispatch(AuthActionCreators.setUser(mockUser))
+				} else {
+					dispatch(AuthActionCreators.setError('bad password or username'))
+				}
+				dispatch(AuthActionCreators.setIsLoading(false))
+			}, 2000)
+		} catch (error) {
+			dispatch(AuthActionCreators.setError('Error in login'))
+		}
+	},
+	logout: () => async (dispatch: AppDispatch) => { }
+}
